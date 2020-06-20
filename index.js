7fa7c753e7973d0fbdf2209c247f316c1e276cdb
Marcus Chiu <marcuschiu9@gmail.com>
2020-06-20 11:59:27 -0500

    initial commit

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ window.onload = function () {
     var verse = document.getElementById("verse");
     var author = document.getElementById("author"); 
 
-    fetch("https://raw.githubusercontent.com/TheRealMarcusChiu/bible-verse-of-the-day-server/master/bvotd.json")
-    .then(response => response.json())
-    .then(function(data) {
-        verse.innerHTML = data.verse;
-        author.innerHTML = data.author;
-    });
+    fetch("https://raw.githubusercontent.com/TheRealMarcusChiu/bible-verse-of-the-day-server/master/bvotd.json", {cache: "no-cache"})
+        .then(response => response.json())
+        .then(function(data) {
+            verse.innerHTML = data.verse;
+            author.innerHTML = data.author;
+        })
+        .catch(function(error) {
+            verse.innerHTML = "What good is it for someone to gain the whole world, yet forfeit their soul?";
+            author.innerHTML = "Mark 8:36 NIV";
+        });
 }
